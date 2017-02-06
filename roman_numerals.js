5bce73ae98511deb1b8ea8ab2df8fcf6fb0f90a3
samuelphsinaga <samuelphsinaga@gmail.com>
2017-02-06 11:29:00 +0700

    case study ..........

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,23 +1,44 @@
 /*
 Please answer the questions below :
+1.  Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
+    Jawaban:
+    semua bisa dijalankan.
+    Solusi no 3 tidak bisa dijalankan karena ada bugs. dengan console log saya bisa memverifikasi hasilnya
 
-1. Apakah semua solusi bisa di eksekusi? Bagaimana caranya mem-verifikasinya?
+2.  Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+    Jawaban:
+     Ada.
+     Roman_numeral_3.js memiliki bug dan cara saya memverifikasi yang dipilih adalah console.log()
 
-2. Apakah ada solusi code yang mengandung *bugs*? Dan bagaimana cara mem-verifikasinya?
+3.   Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
+    Jawaban:
+    no 4, penggunaan modulus tidak terpikirkan sebelumnya
 
-3. Adakah sintaks baru yang belum kamu ketahui? Apakah ada operator baru, expression atau method baru?
+4.  Dapatkah kamu mengerti baris per baris dari semua code yang ada?
+    Jawaban:
+    bisa saya mengerti
 
-4. Dapatkah kamu mengerti baris per baris dari semua code yang ada?
-
-5. Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
+5.  Dapatkah kamu gunakan sintaks baru yang kamu temui di sini di *project* kamu berikutnya?
+    Jawaban:
+    tentu bisa terutama .pop dari array menambah luas logic manipulasi array
 
 6. Adakah struktur data baru? Apa saja, tolong sebutkan
+    Jawaban:
+    saya tidak melihat adanya struktur data yang baru
 
-7. Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
+7.  Apa yang dapat kamu pelajari dari perspektif yang berbeda dari berbagai solution code? Apakah benar-benar berbeda? Jika ya, kenapa berbeda? Debat! Cari tau.
+    Jawaban:
+    Tentu dapat dipelajari tapi butuh waktu untuk mengerti logika diluar logika yang saya pahami
 
 8. Jika kamu harus memilih satu, mana yang menurut kamu yang merupakan solsi terbaik, dan kenapa? Diskusikan dengan teman kelompok kamu tentang hal ini.
+    Jawaban:
+    no 1, karena lebih mudah dimengerti dan lebih simpel
 
 9. Apakah dampak dari pola atau pattern tersebut terhadap performa aplikasi? Bagaimana kamu mengukur dampaknya?
+    Jawab:
+    tentu berakibat aplikasi tidak dapat dijaklankan atau tidak bekerja maksimal apabila masih terdapat bug
 
 10. Apakah dampak dari pola atau pattern terhadap maintenance code? Kenapa kamu bisa yakin?
-*/
+    Jawab:
+    Tentu pola dari code yg kita buat sangat mempengaruhi maintanence code, pola yang readable mempermudah develpoer maintenance dengan maksimal
+  */
